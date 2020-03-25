ebe0295ecfc556a416676947c584771eb34cc6c4
Maxim <pravohranitel@yandex.ru>
2020-03-25 11:00:31 +0200

    add new module, include this module in main file

--- Discriminant_analysis.py
+++ Discriminant_analysis.py
@@ -2,6 +2,7 @@ import PySimpleGUI as sg
 import numpy as np
 from discrim_module import average_vectors as vec
 from discrim_module import covar_matrix as cov_mat
+from discrim_module import decision_making as dec_mak
 
 col_obj1 = [
     [sg.Text('', size=(23, 1)), sg.Text('Образ S\u00b9', size=(10, 1))],
@@ -66,69 +67,6 @@ while True:  # The Event Loop
         #
         # print(evid_s2)
 
-        class Decision_making:
-            '''
-            Этап принятия решения. Вычисления оценки логарифма отношения правдоподобия
-            и подставление найденного значения в решающее правило
-            '''
-
-            def __init__(self, a1, a2, M, n, X):
-                self.a1 = a1
-                self.a2 = a2
-                self.M = M
-                self.n = n
-                self.X = X
-
-            def first_multipler(self):
-                '''
-                Вычисление первого сомножителя. Разность векторов средних и их транспонирование,
-                умножение на обратную ковариационную матрицу
-                '''
-                self.dif_a = self.a1 - self.a2
-                # print(self.a1)
-                # print('---')
-                # print(self.a2)
-                # print('=====')
-                # print(self.dif_a)
-                self.tr_dif_a = np.transpose(self.dif_a)
-                # print(self.tr_dif_a)
-                self.fir_mul = np.dot(self.tr_dif_a, self.M)
-                # print()
-                # print(self.tr_dif_a)
-                # print('*****')
-                # print(self.M)
-                # print('=====')
-                # print(self.fir_mul)
-                return self.fir_mul
-
-            def second_multipler(self):
-                self.X_vec = np.transpose(np.array([self.X]))
-                # print(self.X_vec)
-                self.X_vec = np.dot(self.X_vec, 2)
-                # print(self.X_vec)
-                self.sum_a = self.a1 + self.a2
-                self.sec_mul = self.X_vec - self.sum_a
-                # print(self.sec_mul)
-                return self.sec_mul
-
-            def decision(self):
-                # self.a_t = self.first_multipler()
-                self.a_t = np.transpose(self.first_multipler())
-                self.b_t = self.second_multipler()
-                # print('-----')
-                # print()
-                # print(self.a_t)
-                # print()
-                # print(self.b_t)
-                # print()
-                # print('-----')
-                # print()
-                self.dec = self.a_t * self.b_t
-                self.dec = (self.dec[0]+self.dec[1]+self.dec[2])/2
-                print(self.dec)
-                print('--------------')
-                return self.dec
-
 
 
 
@@ -145,7 +83,7 @@ while True:  # The Event Loop
             # print(b)
             #
             #
-            c = Decision_making(a[0], a[1], b, 1, evid_s3)
+            c = dec_mak.Decision_making(a[0], a[1], b, 1, evid_s3)
             # c1 = c.first_multipler()
             # # print(c1)
             # # print('-------------')
