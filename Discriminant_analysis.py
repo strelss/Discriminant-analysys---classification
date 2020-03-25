30d7555e5c77eb016304883571d5e2068e337bf1
Maxim <pravohranitel@yandex.ru>
2020-03-25 18:43:36 +0200

    finished logic. Yes!

--- Discriminant_analysis.py
+++ Discriminant_analysis.py
@@ -92,19 +92,44 @@ while True:  # The Event Loop
                 # print(self.fir_mul)
                 self.dis_Mahal_quad = self.dif_a[0]*self.fir_mul[0][0] + self.dif_a[1]*self.fir_mul[0][1] + self.dif_a[2]*self.fir_mul[0][2]
                 self.dis_Mahal = round(math.sqrt(self.dis_Mahal_quad), 2)
+                # print(self.dis_Mahal_quad)
                 return self.dis_Mahal
 
 
             def skv(self):              #Расчет стандартного отклонения 1 и 2
                 self.sig_1 = round(math.sqrt((1 / self.m1) + (1 / self.m2)), 2)
                 self.sig_2 = round(math.sqrt((1 / self.m1) + (1 / self.m2) + (4 / self.n)), 2)
+                # print(self.sig_2)
                 return self.sig_1, self.sig_2
 
-            def func_Laplase(self):             #Расчет функции Лапласа
-                pass
+            def func_Laplase_exp(self):
+                '''
+                Расчет функции Лапласа и экспонент, так же возвращает возвращает значения расчета функции Махаланаболиза и стандартных отклонений
+                '''
+                self.d = self.distance_Mahalanobise()
+                self.sig = self.skv()
+                self.sig_1 = self.sig[0]
+                self.sig_2 = self.sig[1]
+                self.F1 = math.erf(self.d / self.sig_1)
+                self.F2 = 1 - math.erf(self.d / self.sig_1)
+                self.F3 = math.erf(self.d / self.sig_2)
+                self.F4 = 1 - math.erf(self.d / self.sig_2)
+                self.exp_1 = round(math.exp(-((self.d ** 2) / 2 * (self.sig_1**2))), 5)
+                self.exp_2 = round(math.exp(-((self.d ** 2) / 2 * (self.sig_2 ** 2))), 5)
+                return self.d, self.sig_1, self.sig_2, self.F1, self.F2, self.F3, self.F4, self.exp_1, self.exp_2
 
-            def exp(self):
-                pass
+            def main(self):
+                '''
+                Главный метод, запускает предыдущие методы и выдает вероятность достоверности рассчитанных данных
+                '''
+                self.all_var = self.func_Laplase_exp()
+                # self.probab_mist = self.F1 * self.F4 + self.F2 * self.F3
+                self.probab_mist = self.all_var[3] * self.all_var[6] + self.all_var[4] * self.all_var[5] + \
+                                   ((self.all_var[1] * self.all_var[2]) / (2.5 * self.all_var[0] * (self.all_var[1] ** 2 - self.all_var[2] ** 2))) * \
+                                   (self.all_var[2] * self.all_var[7] * (self.all_var[5] - self.all_var[6]) - self.all_var[1] * self.all_var[8] * (self.all_var[3] - self.all_var[4]))
+                self.veracity = 1 - self.probab_mist
+                print(self.veracity)
+
 
 
 
@@ -130,8 +155,8 @@ while True:  # The Event Loop
             # print()
             # print(c2)
             d = Diagnostic_assessment(a[0], a[1], b, 4, 5, 1)
-            d = d.skv()
-            print(d)
+            d = d.main()
+            # print(d)
 
         main()
         # sg.popup('Testing complete')
