e2ba435cf80b2476b2ca854ebcacad29dc29cd6e
Maxim <pravohranitel@yandex.ru>
2020-03-26 13:37:02 +0200

    correct GUI

--- Discriminant_analysis.py
+++ Discriminant_analysis.py
@@ -7,42 +7,42 @@ from discrim_module import decision_making as dec_mak
 from discrim_module import diagnostic_assessment as  dia_as
 
 col_obj1 = [
-    [sg.Text('', size=(23, 1)), sg.Text('Образ S\u00b9', size=(10, 1))],
-    [sg.Text('', size=(8, 1)), sg.Text('Объект obj\u2081', size=(10, 1)), sg.Text('Объект obj\u2081', size=(10, 1)),
-     sg.Text('Объект obj\u2081', size=(10, 1)), sg.Text('Объект obj\u2081', size=(10, 1)), ],
-    [sg.Text('Признак x\u2081'), sg.Input('31.33', size=(11, 1), key='s1X1x1'),
-     sg.Input('32.78', size=(11, 1), key='s1X2x1'), sg.Input('35.61', size=(11, 1), key='s1X3x1'), sg.Input('30.54', size=(11, 1), key='s1X4x1'), ],
-    [sg.Text('Признак x\u2082'), sg.Input('4.15', size=(11, 1), key='s1X1x2'),
-     sg.Input('3.34', size=(11, 1), key='s1X2x2'), sg.Input('5.56', size=(11, 1), key='s1X3x2'), sg.Input('4.76', size=(11, 1), key='s1X4x2'), ],
-    [sg.Text('Признак x\u2083'), sg.Input('79', size=(11, 1), key='s1X1x3'), sg.Input('81', size=(11, 1), key='s1X2x3'),
-     sg.Input('82', size=(11, 1), key='s1X3x3'), sg.Input('80', size=(11, 1), key='s1X4x3'), ],
+    [sg.Text('', size=(23, 1)), sg.Text('Образ S\u00b9', size=(10, 1), font=("Helvetica", 15))],
+    [sg.Text('', size=(9, 1)), sg.Text('Объект obj₁', size=(10, 1), font=("Helvetica", 11)), sg.Text('Объект obj₂', size=(10, 1), font=("Helvetica", 11)),
+     sg.Text('Объект obj₃', size=(10, 1), font=("Helvetica", 11)), sg.Text('Объект obj₄', size=(10, 1), font=("Helvetica", 11)), ],
+    [sg.Text('Признак x₁', font=("Helvetica", 11)), sg.Input('31.33', size=(11, 1), key='s1X1x1', font=("Helvetica", 11)),
+     sg.Input('32.78', size=(11, 1), key='s1X2x1', font=("Helvetica", 11)), sg.Input('35.61', size=(11, 1), key='s1X3x1', font=("Helvetica", 11)), sg.Input('30.54', size=(11, 1), key='s1X4x1', font=("Helvetica", 11)), ],
+    [sg.Text('Признак x₂', font=("Helvetica", 11)), sg.Input('4.15', size=(11, 1), key='s1X1x2', font=("Helvetica", 11)),
+     sg.Input('3.34', size=(11, 1), key='s1X2x2', font=("Helvetica", 11)), sg.Input('5.56', size=(11, 1), key='s1X3x2', font=("Helvetica", 11)), sg.Input('4.76', size=(11, 1), key='s1X4x2', font=("Helvetica", 11)), ],
+    [sg.Text('Признак x₃', font=("Helvetica", 11)), sg.Input('79', size=(11, 1), key='s1X1x3', font=("Helvetica", 11)), sg.Input('81', size=(11, 1), key='s1X2x3', font=("Helvetica", 11)),
+     sg.Input('82', size=(11, 1), key='s1X3x3', font=("Helvetica", 11)), sg.Input('80', size=(11, 1), key='s1X4x3', font=("Helvetica", 11)), ],
 ]
 
 col_obj2 = [
-    [sg.Text('', size=(23, 1)), sg.Text('Образ S\u00b2', size=(10, 1))],
-    [sg.Text('', size=(8, 1)), sg.Text('Объект obj\u2081', size=(10, 1)), sg.Text('Объект obj\u2081', size=(10, 1)),
-     sg.Text('Объект obj\u2081', size=(10, 1)), sg.Text('Объект obj\u2081', size=(10, 1)), sg.Text('Объект obj\u2081', size=(10, 1)), ],
-    [sg.Text('Признак x\u2081'), sg.Input('12.25', size=(11, 1), key='s2X1x1'),
-     sg.Input('13.09', size=(11, 1), key='s2X2x1'), sg.Input('7.65', size=(11, 1), key='s2X3x1'), sg.Input('9.98', size=(11, 1), key='s2X4x1'), sg.Input('8.77', size=(11, 1), key='s2X5x1'), ],
-    [sg.Text('Признак x\u2082'), sg.Input('2.98', size=(11, 1), key='s2X1x2'),
-     sg.Input('3.01', size=(11, 1), key='s2X2x2'), sg.Input('2.38', size=(11, 1), key='s2X3x2'),  sg.Input('2.73', size=(11, 1), key='s2X4x2'),  sg.Input('2.91', size=(11, 1), key='s2X5x2'), ],
-    [sg.Text('Признак x\u2083'), sg.Input('66', size=(11, 1), key='s2X1x3'), sg.Input('69', size=(11, 1), key='s2X2x3'),
-     sg.Input('58', size=(11, 1), key='s2X3x3'), sg.Input('71', size=(11, 1), key='s2X4x3'), sg.Input('55', size=(11, 1), key='s2X5x3'), ],
+    [sg.Text('', size=(23, 1)), sg.Text('Образ S\u00b2', size=(10, 1), font=("Helvetica", 15))],
+    [sg.Text('', size=(9, 1)), sg.Text('Объект obj₁', size=(10, 1), font=("Helvetica", 11)), sg.Text('Объект obj₂', size=(10, 1), font=("Helvetica", 11)),
+     sg.Text('Объект obj₃', size=(10, 1), font=("Helvetica", 11)), sg.Text('Объект obj₄', size=(10, 1), font=("Helvetica", 11)), sg.Text('Объект obj₅', size=(10, 1), font=("Helvetica", 11)), ],
+    [sg.Text('Признак x₁', font=("Helvetica", 11)), sg.Input('12.25', size=(11, 1), key='s2X1x1', font=("Helvetica", 11)),
+     sg.Input('13.09', size=(11, 1), key='s2X2x1', font=("Helvetica", 11)), sg.Input('7.65', size=(11, 1), key='s2X3x1', font=("Helvetica", 11)), sg.Input('9.98', size=(11, 1), key='s2X4x1', font=("Helvetica", 11)), sg.Input('8.77', size=(11, 1), key='s2X5x1', font=("Helvetica", 11)), ],
+    [sg.Text('Признак x₂', font=("Helvetica", 11)), sg.Input('2.98', size=(11, 1), key='s2X1x2', font=("Helvetica", 11)),
+     sg.Input('3.01', size=(11, 1), key='s2X2x2', font=("Helvetica", 11)), sg.Input('2.38', size=(11, 1), key='s2X3x2', font=("Helvetica", 11)),  sg.Input('2.73', size=(11, 1), key='s2X4x2', font=("Helvetica", 11)),  sg.Input('2.91', size=(11, 1), key='s2X5x2', font=("Helvetica", 11)), ],
+    [sg.Text('Признак x₃', font=("Helvetica", 11)), sg.Input('66', size=(11, 1), key='s2X1x3', font=("Helvetica", 11)), sg.Input('69', size=(11, 1), key='s2X2x3', font=("Helvetica", 11)),
+     sg.Input('58', size=(11, 1), key='s2X3x3', font=("Helvetica", 11)), sg.Input('71', size=(11, 1), key='s2X4x3', font=("Helvetica", 11)), sg.Input('55', size=(11, 1), key='s2X5x3', font=("Helvetica", 11)), ],
     [sg.Text('', size=(1, 2))]
 ]
 
 research_obj = [
     [sg.Text('', size=(1, 1))],
-    [sg.Text('', size=(1, 1)), sg.Text('Исследуемый объект \u1E8C', size=(20, 1))],
-    [sg.Text('Признак x\u2081'), sg.Input('33.21', size=(11, 1), key='s3x1')],
-    [sg.Text('Признак x\u2082'), sg.Input('5.58', size=(11, 1), key='s3x2')],
-    [sg.Text('Признак x\u2083'), sg.Input('79', size=(11, 1), key='s3x3')],
+    [sg.Text('Исследуемый объект \u1E8C', size=(20, 1), font=("Helvetica", 12))],
+    [sg.Text('Признак x₁', font=("Helvetica", 11)), sg.Input('33.21', size=(11, 1), key='s3x1', font=("Helvetica", 11))],
+    [sg.Text('Признак x₂', font=("Helvetica", 11)), sg.Input('5.58', size=(11, 1), key='s3x2', font=("Helvetica", 11))],
+    [sg.Text('Признак x₃', font=("Helvetica", 11)), sg.Input('79', size=(11, 1), key='s3x3', font=("Helvetica", 11))],
 ]
 
 layout = [
     [sg.Column(col_obj1), sg.Column(research_obj)],
     [sg.Column(col_obj2)],
-    [sg.Output(size=(95, 20))],
+    [sg.Output(size=(105, 20))],
     [sg.Submit('Начать исследование'), sg.Cancel('Выход')]
 ]
 window = sg.Window('Discriminant_classification', layout)
@@ -50,7 +50,6 @@ print('Testing')
 
 while True:  # The Event Loop
     event, values = window.read()
-    # print(event, values) #debug
     if event in (None, 'Выход', 'Cancel'):
         break
     if event == 'Начать исследование':
